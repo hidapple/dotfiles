84601cfbf58b30c365235eef949c2212e0810e44
hidapple <shotfsh@gmail.com>
2017-11-28 21:33:55 +0900

    Add keymap to resize window to fill half of screen

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,5 +1,7 @@
--- Maximize window size: Command + Option + ↑
-hs.hotkey.bind({"cmd", "alt"}, "UP", function()
+local prefix = {"cmd", "ctrl"}
+
+-- Maximize window size
+hs.hotkey.bind(prefix, "Up", function()
   local window = hs.window.focusedWindow()
   local frame = window:frame()
   local screen = window:screen()
@@ -8,6 +10,36 @@ hs.hotkey.bind({"cmd", "alt"}, "UP", function()
   frame.x = max.x
   frame.y = max.y
   frame.w = max.w
+  frame.h = max.h
+
+  window:setFrame(frame)
+end)
+
+-- Resize window to fill half of left side of screen
+hs.hotkey.bind(prefix, "Left", function()
+  local window = hs.window.focusedWindow()
+  local frame = window:frame()
+  local screen = window:screen()
+  local max = screen:frame()
+
+  frame.x = max.x
+  frame.y = max.y
+  frame.w = max.w / 2
+  frame.h = max.h
+
+  window:setFrame(frame)
+end)
+
+-- Resize window to fill half of right side of screen
+hs.hotkey.bind(prefix, "Right", function()
+  local window = hs.window.focusedWindow()
+  local frame = window:frame()
+  local screen = window:screen()
+  local max = screen:frame()
+
+  frame.x = max.x + max.w / 2
+  frame.y = max.y
+  frame.w = max.w / 2
   frame.h = max.h
 
   window:setFrame(frame)
