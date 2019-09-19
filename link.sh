7320c6e65d7a1dd2d636b824fa352945bb96752a
hidapple <shotfsh@gmail.com>
2019-09-19 10:37:38 +0900

    Update zsh config

--- link.sh
+++ link.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
+DOT="$HOME/.dotfiles"
+
+
 cd `dirname $0`
 for f in .??*
 do
   if [ $f == ".git" ]; then continue; fi
   if [ $f == ".DS_Store" ]; then continue; fi
 
-  ln -snv ~/.dotfiles/$f ~/$f
+  ln -snv $DOT/$f $HOME/$f
 done
 
+# zsh
+# -----------
+ln -snv $DOT/zsh/zshrc   $HOME/.zshrc
+ln -snv $DOT/zsh/fzf     $HOME/.fzf
+ln -snv $DOT/zsh/fzf.zsh $HOME/.fzf.zsh
+
 # neovim
+# -----------
 if [ "$XDG_CONFIG_HOME" == "" ]; then XDG_CONFIG_HOME=$HOME/.config; fi
 mkdir -p $XDG_CONFIG_HOME/nvim
-ln -snv ~/.vim $XDG_CONFIG_HOME/nvim
-ln -snv ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+ln -snv $HOME/.vim   $XDG_CONFIG_HOME/nvim
+ln -snv $HOME/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
