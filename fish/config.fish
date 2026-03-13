4c3d405906622e1e05f312b9db2ff78be064b7d1
hidapple <shotfsh@gmail.com>
2026-03-13 17:15:43 +0900

    replace fish alias by abbr

--- fish/config.fish
+++ fish/config.fish
@@ -4,35 +4,30 @@ set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx GOPATH $HOME/dev
 set -gx LS_COLORS 'di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 
-# Homebrew
-eval (/opt/homebrew/bin/brew shellenv)
-
 # PATH
 fish_add_path $HOME/.local/bin $HOME/.cargo/bin $GOPATH/bin
 
-# mise
-mise activate fish | source
+# Homebrew
+eval (/opt/homebrew/bin/brew shellenv)
 
-# Aliases
-alias ls "ls -GF"
-alias ll "ls -l"
-alias cl clear
-alias vim nvim
-alias docc "docker compose"
-alias k kubectl
-alias cc 'tmux split-window -v -p 40 && tmux send-keys "claude" Enter'
+# Abbreviations
+abbr -a ls 'ls -GF'
+abbr -a ll 'ls -l -GF'
+abbr -a cl clear
+abbr -a vim nvim
+abbr -a docc 'docker compose'
+abbr -a k kubectl
+abbr -a cc 'tmux split-window -v -p 40 && tmux send-keys "claude" Enter'
+
+# Keybindings
+bind \cg ghq_fzf
 
 # No beep
 set -g fish_greeting
 
-# direnv
-direnv hook fish | source
-
-# fzf
+# source
+mise activate fish | source
 fzf --fish | source
-
-# Keybindings
-bind \cg ghq_fzf
 
 # Local config
 if test -f $XDG_CONFIG_HOME/fish/config.local.fish
